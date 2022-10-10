a416f8e0dcf47f8a458593279ca1e93bd85e3bb7
Gm_Abhishek <gmabhishek2004@gmail.com>
2022-10-10 12:10:26 +0530

    CAll_App_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'Contacts.dart';
+import 'Contacts_remastered.dart';
 import 'package:text_scroll/text_scroll.dart';
+import 'dart:developer';
 
 void main() async {
   runApp(const MyApp());
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+List<String> recipents = ["9876543210", "8765432190"];
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -46,12 +47,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getContact();
-  }
+  set value(value) {}
+  bool selectingmode = false;
 
   void getContact() async {
     if (await FlutterContacts.requestPermission()) {
@@ -60,88 +57,101 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {});
     }
   }
-  void actionPopUpItemSelected(String value, String stu_num, String par_num) {
-    if (value == 'stud') {
-      launch('tel: ${stu_num}');
-      // You can navigate the user to edit page.
-    } else if (value == 'Par') {
-      launch('tel: ${par_num}');
-
-      // You can delete the item.
-    }
-  }
+    /*** void _sendSMS(String message, List<String> recipents) async {
+    String _result = await sendSMS(message: message, recipients: recipents)
+        .catchError((onError) {
+      print(onError);
+    });
+    print(_result);
+  }***/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          centerTitle: true,
+          leading: selectingmode
+              ? IconButton(
+                  icon: const Icon(Icons.arrow_back_ios_new),
+                  onPressed: () {
+                    setState(() {
+                      selectingmode = false;
+                      contacts.forEach((p) => p.selected = false);
+                    });
+                  },
+                )
+              : null,
           title: const Text(
             "II IT-A Contacts",
             style: TextStyle(
               shadows: <Shadow>[
                 Shadow(
-                  offset: Offset(2.0, 2.0),
+                  offset: Offset(5.0, -2.0),
                   blurRadius: 8.0,
                   color: Color.fromARGB(255, 255, 255, 255),
                 ),
                 Shadow(
-                  offset: Offset(2.0, 2.0),
+                  offset: Offset(5.0, -2.0),
                   blurRadius: 5.0,
                   color: Color.fromARGB(255, 255, 255, 255),
                 ),
               ],
               fontSize: 36,
               fontFamily: 'Booter-Zero-zero',
-              fontWeight: FontWeight.w700,
+              fontWeight: FontWeight.w500,
               color: Color.fromARGB(255, 0, 0, 0),
             ),
           ),
-          centerTitle: true,
-          backgroundColor: Colors.pinkAccent.withOpacity(0.7),
-          elevation: 0,
+          backgroundColor: Color.fromRGBO(0,0,0,1),
         ),
-        body: (contacts) == null
-            ? Center(child: Icon(Icons.lens_outlined))
-            : ListView.builder(
-                itemCount: contacts!.length,
+        body:  ListView.builder(
+                itemCount: contacts.length,
                 itemBuilder: (BuildContext context, int index) {
-                  String name = contacts![index].displayName.isEmpty
-                      ? "Invalid Name"
-                      : contacts![index].displayName;
-                  String stu_num = (contacts![index].phones.isNotEmpty)
-                      ? (contacts![index].phones.first.number)
-                      : '--';
-                  String par_num = (contacts![index].phones.isNotEmpty)
-                      ? (contacts![index].name.middle)
-                      : '--';
                   return ListTile(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    leading: CircleAvatar(
-                        radius: 24,
-                        child: Text(
-                          '${index + 1}',
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontFamily: 'Booter-Zero-zero',
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromRGBO(47, 48, 48, 1),
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(2.0, 2.0),
-                                blurRadius: 5.0,
-                                color: Color.fromARGB(255, 255, 255, 255),
-                              ),
-                              Shadow(
-                                offset: Offset(2.0, 2.0),
-                                blurRadius: 2.0,
-                                color: Color.fromARGB(255, 255, 255, 255),
-                              ),
-                            ],
-                          ),
-                        )),
+                    onLongPress: () {
+                      setState(() {
+                        selectingmode = true;
+                      });
+                    },
+                    onTap: () {
+                      setState(() {
+                        if (selectingmode) {
+                          contacts[index].selected =
+                              !contacts[index].selected;
+                          log(contacts[index].selected.toString());
+                        }
+                      });
+                    },
+                    leading: GestureDetector(
+                      behavior: HitTestBehavior.opaque,
+                      onTap: () {},
+                      child: CircleAvatar(
+                          radius: 24,
+                          child: Text(
+                            '${index + 1}',
+                            style: TextStyle(
+                              fontSize: 50,
+                              fontFamily: 'Booter-Zero-zero',
+                              fontWeight: FontWeight.w600,
+                              color: Color.fromRGBO(47, 48, 48, 1),
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(2.0, 2.0),
+                                  blurRadius: 5.0,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                ),
+                                Shadow(
+                                  offset: Offset(2.0, 2.0),
+                                  blurRadius: 2.0,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                ),
+                              ],
+                            ),
+                          )
+                      ),
+                    ),
                     title: TextScroll(
-                      "${name}",
+                      "${contacts[index].name}",
                       style: TextStyle(
                         fontSize: 16,
                         fontFamily: 'JosefinSans-Italic-VariableFont_wght',
@@ -156,58 +166,59 @@ class _MyHomePageState extends State<MyHomePage> {
                       textAlign: TextAlign.left,
                       selectable: true,
                     ),
-                    trailing: PopupMenuButton(
-                      constraints: BoxConstraints(
-                          minHeight: 0,
-                          minWidth: 50,
-                          maxHeight: double.infinity,
-                          maxWidth: double.infinity),
-                      // child: Container(
-                      child: Icon(
-                        Icons.add_call,
-                        color: Color.fromRGBO(93, 97, 97, 1),
-                        size: 36,
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)
-                              .copyWith(topRight: Radius.circular(0))),
-                      elevation: 10,
-                      color: Colors.grey.shade300,
-                      itemBuilder: (context) {
-                        return [
-                          PopupMenuItem(
-                            value: 'stud',
-                            child: TextScroll(
-                              'Student: ${name}',
-                              mode: TextScrollMode.bouncing,
-                              velocity:
-                                  Velocity(pixelsPerSecond: Offset(25, 0)),
-                              delayBefore: Duration(milliseconds: 1000),
-                              numberOfReps: 50,
-                              pauseBetween: Duration(milliseconds: 150),
-                              selectable: true,
-                              textAlign: TextAlign.center,
+                    trailing: (selectingmode)
+                        ? ((contacts[index].selected)
+                            ? Icon(Icons.check_box)
+                            : Icon(Icons.check_box_outline_blank))
+                        : PopupMenuButton(
+                            constraints: BoxConstraints(
+                                minHeight: 0,
+                                minWidth: 50,
+                                maxHeight: double.infinity,
+                                maxWidth: double.infinity),
+                            child: Icon(
+                              Icons.add_call,
+                              color: Color.fromRGBO(93, 97, 97, 1),
+                              size: 36,
                             ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)
+                                    .copyWith(topRight: Radius.circular(0))),
+                            elevation: 10,
+                            color: Colors.grey.shade300,
+                            itemBuilder: (context) {
+                              return [
+                                PopupMenuItem(
+                                  onTap: (){ launch('tel: ${contacts[index].stu_num}');},
+                                  child:  TextScroll(
+                                    'Student: ${contacts[index].name}',
+                                    mode: TextScrollMode.bouncing,
+                                    velocity: Velocity(
+                                        pixelsPerSecond: Offset(25, 0)),
+                                    delayBefore: Duration(milliseconds: 1000),
+                                    numberOfReps: 50,
+                                    pauseBetween: Duration(milliseconds: 150),
+                                    selectable: true,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                PopupMenuItem(
+                                  onTap: (){ launch('tel: ${contacts[index].par_num}');},
+                                  child: TextScroll(
+                                    'Parent:  ${contacts[index].parent}',
+                                    mode: TextScrollMode.bouncing,
+                                    velocity: Velocity(
+                                        pixelsPerSecond: Offset(25, 0)),
+                                    delayBefore: Duration(milliseconds: 1000),
+                                    numberOfReps: 50,
+                                    pauseBetween: Duration(milliseconds: 150),
+                                    selectable: true,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ];
+                            },
                           ),
-                          PopupMenuItem(
-                            value: 'Par',
-                            child: TextScroll(
-                              'Parent:  ${contacts![index].name.first}',
-                              mode: TextScrollMode.bouncing,
-                              velocity:
-                                  Velocity(pixelsPerSecond: Offset(25, 0)),
-                              delayBefore: Duration(milliseconds: 1000),
-                              numberOfReps: 50,
-                              pauseBetween: Duration(milliseconds: 150),
-                              selectable: true,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ];
-                      },
-                      onSelected: (String value) =>
-                          actionPopUpItemSelected(value, stu_num, par_num),
-                    ),
                   );
                 },
               ));
